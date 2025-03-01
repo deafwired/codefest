135879a818c5ab2a4c9187b64a06b090048c1523
Matt <deafwired@proton.me>
2025-03-01 16:24:55 -0500

    api has name

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import os
 
 if __name__ == "__main__":
     for filename in os.listdir("./testFiles/"):
-        path = os.path.join("./testFiles",filename)
+        path = os.path.join("./testFiles", filename)
         fp = FileParser()
         api = API()
-        print(f"{filename}: {api.summary(fp.extract_text(path))}")
+        summary = api.summary(fp.extract_text(path))
+        print(f"{filename}: {summary}")
+        print(f"{filename} new name: {api.name(summary)}")
--- util/api.py
+++ util/api.py
@@ -1,19 +1,34 @@
 from keybert import KeyBERT
+import json
+import requests
+
 
 class API:
     def __init__(self):
         self.kw_model = KeyBERT()
-    
-    def queryAi(self, prompt):
+
+    def keywords(self, prompt):
         keywords = self.kw_model.extract_keywords(
-            prompt,
-            keyphrase_ngram_range=(1, 1),
-            stop_words="english",
-            top_n=20
+            prompt, keyphrase_ngram_range=(1, 1), stop_words="english", top_n=20
         )
         # Return only the keywords separated by commas
-        return ','.join([word for word, score in keywords])
-    
+        return ",".join([word for word, score in keywords])
+
     def summary(self, prompt):
         # extract keywords using queryAi
-        return self.queryAi(prompt[:8000])
+        return self.keywords(prompt[:8000])
+
+    def name(self, prompt):
+        url = "http://localhost:11434/api/generate"
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "model": "gemma:7b",
+            "prompt": "Create only a filename with no description based on these keywords:\n"
+            + prompt,
+            "stream": False,
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        if response.status_code == 200:
+            return response.json().get("response", "No response recieved")
+        else:
+            return f"Error {response.status_code}: {response.text}"
