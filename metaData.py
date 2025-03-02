f0da2195fc1c8964ebd3fad78d748cbe64938e9e
drewfisto <andrewlbrown06@gmail.com>
2025-03-01 23:10:32 -0500

    Moved logo to images folder

--- metaData.py
+++ metaData.py
@@ -7,8 +7,6 @@ from FileParser import FileParser
 from util.api import API
 
 class MetaData:
-
-
     def appendCSV(self, new_file_path, file_path, key_words):
         with open(new_file_path, "a") as file:
             file.write(file_path + ", ")
