565fbcd819a57496c2582b93d0ad246b8aba92c0
Matt <deafwired@proton.me>
2025-03-02 01:25:50 -0500

    updating test

--- test.py
+++ test.py
@@ -5,17 +5,16 @@ import json
 from search import QueryIndex
 
 if __name__ == "__main__":
-    for filename in os.listdir("./testFiles/"):
-        path = os.path.join("./testFiles", filename)
-        fp = FileParser()
-        api = API()
-        # summary = api.summary(fp.extract_text(path))
-        # print(f"{filename}: {summary}")
-        # print(f"{filename} new name: {api.name(summary)}")
-        query = "I am looking for an english syllabus" # Test query
-        keywords = api.queryToKeywords(query)
-        qi = QueryIndex()
-        search_results = qi.generate_search(keywords, "data.csv")
-        print(search_results)
+    # for filename in os.listdir("./testFiles/"):
+    fp = FileParser()
+    api = API()
+    # summary = api.summary(fp.extract_text(path))
+    # print(f"{filename}: {summary}")
+    # print(f"{filename} new name: {api.name(summary)}")
+    query = "I am looking for an english syllabus"  # Test query
+    keywords = api.queryToKeywords(query)
+    qi = QueryIndex()
+    search_results = qi.generate_search(keywords, "data.csv")
+    print(search_results)
 
-        #print(f"{keywords}")
+    # print(f"{keywords}")
