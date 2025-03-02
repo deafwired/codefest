b1b2ba00419333178a907b46d52888bdf39834c1
TheMightyd7 <dd@davisinfo.com>
2025-03-02 09:48:38 -0500

    Searching in GUI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fileinput import filename
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QListWidget
+from util.api import API
 from search import QueryIndex
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
-    QListWidget
 
 class SearchApp(QWidget):
     def __init__(self):
@@ -10,7 +9,6 @@ class SearchApp(QWidget):
         self.setWindowTitle("Search GUI")
         self.setGeometry(200, 200, 800, 500)
 
-        # Layout
         layout = QVBoxLayout()
 
         # Header
@@ -18,26 +16,31 @@ class SearchApp(QWidget):
         self.header.setStyleSheet("font-size: 20px; font-weight: bold;")
         layout.addWidget(self.header)
 
-        # Search Bar
         search_layout = QHBoxLayout()
         self.search_input = QLineEdit()
         self.search_button = QPushButton("Search")
         self.search_button.clicked.connect(self.perform_search)
-
         search_layout.addWidget(self.search_input)
         search_layout.addWidget(self.search_button)
-
         layout.addLayout(search_layout)
 
-        # Search Results
         self.result_list = QListWidget()
         layout.addWidget(self.result_list)
 
         self.setLayout(layout)
 
-    def perform_search(self, filename, query):
-        pass
+    def perform_search(self):
+        query = self.search_input.text()
+        
+        api = API()
+        keywords = api.queryToKeywords(query)
+        
+        qi = QueryIndex()
+        search_results = qi.generate_search(keywords, "data.csv")
 
+        self.result_list.clear()
+        for result in search_results:
+            self.result_list.addItem(result)
 
 if __name__ == "__main__":
     app = QApplication([])
