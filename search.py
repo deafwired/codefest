263e6a03e80c64add26d01851040148fe5b11c34
drewfisto <andrewlbrown06@gmail.com>
2025-03-02 01:16:04 -0500

    Added the search.py file to get search reults

--- search.py
+++ search.py
@@ -1,12 +1,22 @@
 class QueryIndex:
-    def generate_index(self, filename):
-        index = {}
+    def generate_search(self, search, filename):
+        results = {}
 
         with open(filename, "r") as file:
             for line in file:
                 line = line.strip("\n")
+                line = line.lower()
                 split = line.split(",")
                 file_path = split[0]
-                keywords = split[1:]
-                index[file_path] = keywords
-        print(index)
+                key_words = split[1:]
+
+                score = sum(1 for word in search if word in key_words)  # Count matches
+
+                if score > 0:  # Fix: Ensure at least one match
+                    results[file_path] = score
+
+            sorted_searches = dict(
+                sorted(results.items(), key=lambda item: item[1], reverse=True))  # Sort in descending order
+            return sorted_searches
+
+
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from FileParser import FileParser
 from util.api import API
 import os
 import json
-
+from search import QueryIndex
 
 if __name__ == "__main__":
     for filename in os.listdir("./testFiles/"):
@@ -12,8 +12,10 @@ if __name__ == "__main__":
         # summary = api.summary(fp.extract_text(path))
         # print(f"{filename}: {summary}")
         # print(f"{filename} new name: {api.name(summary)}")
-        query = "Tax docs" # Test query
-        keywords = api.queryToKeywords(query) # Turn query into keywords
-        
+        query = "I am looking for an english syllabus" # Test query
+        keywords = api.queryToKeywords(query)
+        qi = QueryIndex()
+        search_results = qi.generate_search(keywords, "data.csv")
+        print(search_results)
 
-        print(f"{keywords}")
+        #print(f"{keywords}")
